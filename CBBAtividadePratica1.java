1c84ef0b88c4e5245e6140ec3767f0ac27aed1c5
Thierry Silva <thierryfulgencio@gmail.com>
2022-11-05 22:00:40 -0300

    Implementação das 4 questões da atividade em equipe 7.03

---
+++ CBBAtividadePratica1.java
@@ -0,0 +1,11 @@
+public class CBBAtividadePratica1 {
+
+  public static void main(String[] args) {
+    int[] array = new int[100];
+
+    for (int i = 0; i < 100; i++) {
+      array[i] = i;
+      System.out.print(array[i] + " ");
+    }
+  }
+}
---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,11 @@
+public class CBBAtividadePratica4 {
+
+  public static void main(String[] args) {
+    int[] array = new int[100];
+
+    for (int i = 0; i < 101; i++) {
+      array[i] = i;
+      System.out.print(array[i] + " ");
+    }
+  }
+}
